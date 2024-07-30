0dd3851e745c472e42a77867c2ce8be627dc0e27
hikisenrii <legendabandaralama@gmail.com>
2024-07-30 22:17:38 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+
+TOKEN = os.getenv('7211989606:AAHQllK1z8URxGEDgSUSEV2HEWx2IBTN8oU')
+OWNER_ID = int(os.getenv('127385539'))
+API_ID = int(os.getenv('29225932'))
+API_HASH = os.getenv('90665d507ab817ec8b32fd02c4eb8d72')
